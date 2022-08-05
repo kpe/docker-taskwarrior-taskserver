9aa8e22fb508efa60d55708caf409bf1e3f7f2ee
kpe <kpe@users.noreply.github.com>
2022-08-05 23:44:03 +0200

    adding --data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,10 +16,10 @@ fi;
 
 if [ ! -f $TASKDATA/config ]; then
     echo "===> $TASKDATA/config not found. Initializing taskd."
-    execute taskd init
-    execute taskd config --force log $TASKDATA/taskd.log
-    execute taskd config --force pid.file /taskd.pid
-    execute taskd config --force server 0.0.0.0:53589
+    execute taskd init   --data $TASKDATA
+    execute taskd config --data $TASKDATA --force log $TASKDATA/taskd.log
+    execute taskd config --data $TASKDATA --force pid.file /taskd.pid
+    execute taskd config --data $TASKDATA --force server 0.0.0.0:53589
 fi;
 
 
@@ -28,20 +28,20 @@ if [ ! -f $TASKDATA/pki/ca.cert.pem ]; then
     cd $TASKDATA/pki
     execute ./generate
 
-    execute taskd config --force client.cert $TASKDATA/pki/client.cert.pem
-    execute taskd config --force client.key  $TASKDATA/pki/client.key.pem
-    execute taskd config --force server.cert $TASKDATA/pki/server.cert.pem
-    execute taskd config --force server.key  $TASKDATA/pki/server.key.pem
-    execute taskd config --force server.crl  $TASKDATA/pki/server.crl.pem
-    execute taskd config --force ca.cert     $TASKDATA/pki/ca.cert.pem
+    execute taskd config --data $TASKDATA --force client.cert $TASKDATA/pki/client.cert.pem
+    execute taskd config --data $TASKDATA --force client.key  $TASKDATA/pki/client.key.pem
+    execute taskd config --data $TASKDATA --force server.cert $TASKDATA/pki/server.cert.pem
+    execute taskd config --data $TASKDATA --force server.key  $TASKDATA/pki/server.key.pem
+    execute taskd config --data $TASKDATA --force server.crl  $TASKDATA/pki/server.crl.pem
+    execute taskd config --data $TASKDATA --force ca.cert     $TASKDATA/pki/ca.cert.pem
 else
     echo '===> Certificates already exist'
 fi;
 
 if [ ! -f $TASKDATA/pki/default-client.key.pem ]; then
     echo '===> No users setup yet. Setting up organization Default with user Default'
-    execute taskd add org Default
-    execute taskd add user Default Default
+    execute taskd add --data $TASKDATA  org Default
+    execute taskd add --data $TASKDATA user Default Default
     cd $TASKDATA/pki
     ./generate.client default-client
 fi;
